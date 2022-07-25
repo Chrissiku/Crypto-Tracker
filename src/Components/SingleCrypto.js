5066a719b0ccc8dc729bfe1877b347c8443e97d7
chrissiku <chrissiku5@gmail.com>
2022-07-25 23:36:58 +0200

    Implement the currencyFormatter function

--- src/Components/SingleCrypto.js
+++ src/Components/SingleCrypto.js
@@ -1,19 +1,26 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable object-curly-newline */
 /* eslint-disable react/self-closing-comp */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable quotes */
-
 import { useSelector } from "react-redux/es/exports";
 import { useParams, Link } from "react-router-dom";
 import Icon from "react-icons-kit";
 import { arrowCircleLeft } from "react-icons-kit/fa/arrowCircleLeft";
+
+const CurrencyFormatter = (amount) =>
+  new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(amount);
 
 const SingleCrypto = () => {
   const params = useParams();
   const myState = useSelector((state) => state);
   const CurrentCrypto = myState.find((crypto) => crypto.id === params.cryptoId);
   const { id, icon, name, symbol, rank, price } = CurrentCrypto;
+
   return (
     <>
       <div>
@@ -29,7 +36,7 @@ const SingleCrypto = () => {
         <span>{symbol}</span>
         <span>{name}</span>
         <span>{rank}</span>
-        <span>{price}</span>
+        <span>{CurrencyFormatter(price)}</span>
       </div>
     </>
   );
