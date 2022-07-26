04afc13ea97c6679ab659bdfdbac630e3a1ff8da
chrissiku <chrissiku5@gmail.com>
2022-07-26 16:00:04 +0200

    Add style to the single crypto page

--- src/Components/SingleCrypto.js
+++ src/Components/SingleCrypto.js
@@ -8,6 +8,8 @@ import { useSelector } from "react-redux/es/exports";
 import { useParams, Link } from "react-router-dom";
 import Icon from "react-icons-kit";
 import { arrowCircleLeft } from "react-icons-kit/fa/arrowCircleLeft";
+import { bitcoin } from "react-icons-kit/fa/bitcoin";
+import { twitter } from "react-icons-kit/fa/twitter";
 
 const CurrencyFormatter = (amount) =>
   new Intl.NumberFormat("en-US", {
@@ -19,17 +21,34 @@ const SingleCrypto = () => {
   const params = useParams();
   const myState = useSelector((state) => state);
   const CurrentCrypto = myState.find((crypto) => crypto.id === params.cryptoId);
-  const { id, icon, name, symbol, rank, price } = CurrentCrypto;
+  const {
+    id,
+    icon,
+    name,
+    symbol,
+    rank,
+    price,
+    priceBtc,
+    volume,
+    marketCap,
+    availableSupply,
+    totalSupply,
+    priceChange1h,
+    priceChange1d,
+    priceChange1w,
+    websiteUrl,
+    twitterUrl,
+  } = CurrentCrypto;
 
   return (
     <>
       <div className="single-crypto">
         <Link to="/">
-          <i>
+          <i className="go-back">
             <Icon icon={arrowCircleLeft} size={25} />
           </i>
         </Link>
-        <div className="single-crypto-infos">
+        <div key={id} className="single-crypto-infos">
           <div className="banner">
             <div className="banner-img">
               <img src={icon} alt={name} />
@@ -37,12 +56,93 @@ const SingleCrypto = () => {
                 Rank :&nbsp;
                 {rank}
               </span>
+              <span className="price">{CurrencyFormatter(price)}</span>
+              <span className="crypto-name">
+                {name}
+                {' '}
+                (
+                {symbol}
+                )
+              </span>
             </div>
           </div>
-          <span>{id}</span>
-          <span>{symbol}</span>
-          <span>{name}</span>
-          <span>{CurrencyFormatter(price)}</span>
+          <div className="crypto-info">
+            <div className="card-info">
+              <span>
+                Rank :
+                {' '}
+                {rank}
+              </span>
+              <span>
+                Symbol :
+                {' '}
+                {symbol}
+              </span>
+              <span>
+                Name :
+                {' '}
+                {name}
+              </span>
+              <span>
+                price/qty :
+                {' '}
+                {CurrencyFormatter(price)}
+                {' '}
+                /
+                {' '}
+                {priceBtc}
+              </span>
+              <span>
+                Volume :
+                {' '}
+                {volume}
+              </span>
+              <span>
+                Market-cap :
+                {' '}
+                {CurrencyFormatter(marketCap)}
+              </span>
+            </div>
+            <div className="card-info">
+              <span>
+                Av. Supply :
+                {' '}
+                {availableSupply}
+              </span>
+              <span>
+                Tot. Sypply :
+                {' '}
+                {totalSupply}
+              </span>
+              <span>
+                price var/H :
+                {' '}
+                {priceChange1h}
+                % /hour
+              </span>
+              <span>
+                price var/D :
+                {' '}
+                {priceChange1d}
+                % /day
+              </span>
+              <span>
+                price var/W :
+                {' '}
+                {priceChange1w}
+                % /Week
+              </span>
+              <span className="web-icons">
+                Get More infos :
+                <a href={websiteUrl} target="_blank" rel="noreferrer">
+                  <Icon icon={bitcoin} size={20} />
+                </a>
+                <a href={twitterUrl} target="_blank" rel="noreferrer">
+                  <Icon icon={twitter} size={20} />
+                </a>
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </>
