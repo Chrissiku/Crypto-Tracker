76b702927ac4021cf22e88cc1faa5105ab2093e3
chrissiku <chrissiku5@gmail.com>
2022-07-26 16:40:45 +0200

    Write test for the Header component

---
+++ src/__tests__/header.test.js
@@ -0,0 +1,20 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import TestRenderer from "react-test-renderer";
+import { Provider } from "react-redux";
+import store from "../Redux/configureStore";
+import Header from "../Components/Header";
+
+it("Test if header render correctly", () => {
+  const TREE = TestRenderer.create(
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    </Provider>
+  );
+  expect(TREE).toMatchSnapshot();
+});
